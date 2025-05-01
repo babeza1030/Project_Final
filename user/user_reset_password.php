58c497b1ec613516ea6315056420c194f82610f3
babeza1030 <78146664+babeza1030@users.noreply.github.com>
2025-05-02 01:55:23 +0700

    Create user_reset_password.php

---
+++ user/user_reset_password.php
@@ -0,0 +1,12 @@
+
+     <script>
+        function validateForm() {
+            var password = document.getElementById("password").value;
+            var confirmPassword = document.getElementById("confirm_password").value;
+            if (password !== confirmPassword) {
+                alert("Passwords do not match.");
+                return false;
+            }
+            return true;
+        }
+    </script>
